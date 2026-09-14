54fe08ac6c4828dfb9e881a75110ece47d594260
arousso59-prog <arousso59@icloud.com>
2026-09-14 13:04:27 +0200

    Refactor: implement image provider base class

---
+++ components/geometrie_camera_app/image_provider.cpp
@@ -0,0 +1,11 @@
+#include "image_provider.h"
+
+namespace esphome {
+namespace geometrie_camera_app {
+
+ImageProvider::ImageProvider() {}
+
+ImageProvider::~ImageProvider() {}
+
+}  // namespace geometrie_camera_app
+}  // namespace esphome
