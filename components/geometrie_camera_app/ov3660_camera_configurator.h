bd881b2614df9090f75033d197aee28c38a9e268
arousso59-prog <arousso59@icloud.com>
2026-09-14 17:38:53 +0200

    refactor OV3660 configurator for PCLK divider diagnostic

--- components/geometrie_camera_app/ov3660_camera_configurator.h
+++ components/geometrie_camera_app/ov3660_camera_configurator.h
@@ -9,22 +9,27 @@ class Ov3660CameraConfigurator {
  public:
   Ov3660CameraConfigurator();
 
+  void set_pclk_divider(uint8_t divider);
   void setup();
   void loop();
 
   bool sensor_detected() const;
-  bool pclk_test_applied() const;
-  int original_clock_pol_control() const;
-  int modified_clock_pol_control() const;
+  bool pclk_divider_applied() const;
+  uint8_t requested_pclk_divider() const;
+  int original_pclk_ratio() const;
+  int applied_pclk_ratio() const;
+  int vfifo_ctrl0c() const;
 
  private:
-  bool apply_pclk_polarity_test_();
+  bool apply_pclk_divider_();
 
+  uint8_t requested_pclk_divider_;
   bool sensor_detected_;
-  bool pclk_test_applied_;
+  bool pclk_divider_applied_;
   uint32_t last_attempt_ms_;
-  int original_clock_pol_control_;
-  int modified_clock_pol_control_;
+  int original_pclk_ratio_;
+  int applied_pclk_ratio_;
+  int vfifo_ctrl0c_;
 };
 
 }  // namespace geometrie_camera_app
