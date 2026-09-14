bf367eb4fc17497c5435cd3a7683defee5296604
arousso59-prog <arousso59@icloud.com>
2026-09-14 12:16:06 +0200

    Ajoute composant ESPHome geometrie_camera_app

---
+++ components/geometrie_camera_app/__init__.py
@@ -0,0 +1,24 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.const import CONF_ID
+
+
+geometrie_camera_app_ns = cg.esphome_ns.namespace("geometrie_camera_app")
+
+GeometrieCameraApp = geometrie_camera_app_ns.class_(
+    "GeometrieCameraApp",
+    cg.Component,
+)
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(GeometrieCameraApp),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
