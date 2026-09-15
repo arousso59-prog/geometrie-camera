4fe1e20901f93b5d0473a682aabadcb53545f0c0
arousso59-prog <arousso59@icloud.com>
2026-09-15 15:01:14 +0200

    Add resolution parameter support to target search API

--- components/geometrie_camera_app/target_search_diagnostic_api.h
+++ components/geometrie_camera_app/target_search_diagnostic_api.h
@@ -5,23 +5,27 @@
 namespace esphome {
 namespace geometrie_camera_app {
 
+class CameraResolutionController;
 class GrayscaleDiagnostic;
 class TargetSearchDiagnostic;
 
 class TargetSearchDiagnosticApiHandler : public AsyncWebHandler {
  public:
-  TargetSearchDiagnosticApiHandler(TargetSearchDiagnostic *diagnostic, GrayscaleDiagnostic *visualization);
+  TargetSearchDiagnosticApiHandler(TargetSearchDiagnostic *diagnostic, GrayscaleDiagnostic *visualization,
+                                   CameraResolutionController *resolution_controller);
 
   bool canHandle(AsyncWebServerRequest *request) const override;
   void handleRequest(AsyncWebServerRequest *request) override;
 
  private:
+  bool apply_requested_resolution_(AsyncWebServerRequest *request);
   void handle_search_(AsyncWebServerRequest *request);
   void handle_status_(AsyncWebServerRequest *request);
   void handle_image_(AsyncWebServerRequest *request);
 
   TargetSearchDiagnostic *diagnostic_;
   GrayscaleDiagnostic *visualization_;
+  CameraResolutionController *resolution_controller_;
 };
 
 }  // namespace geometrie_camera_app
