b6dfe8aac0906245a9a1d01e0fbc11130f588a9d
arousso59-prog <arousso59@icloud.com>
2026-09-16 07:04:03 +0200

    Add runtime diagnostics HTTP handler interface

---
+++ components/geometrie_camera_app/runtime_diagnostics_api.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "esphome/components/web_server_base/web_server_base.h"
+
+namespace esphome {
+namespace geometrie_camera_app {
+
+class GrayscaleDiagnostic;
+class RuntimeDiagnostics;
+class TargetSearchDiagnostic;
+
+class RuntimeDiagnosticsApiHandler : public AsyncWebHandler {
+ public:
+  RuntimeDiagnosticsApiHandler(RuntimeDiagnostics *runtime, GrayscaleDiagnostic *grayscale,
+                               TargetSearchDiagnostic *target_search);
+
+  bool canHandle(AsyncWebServerRequest *request) const override;
+  void handleRequest(AsyncWebServerRequest *request) override;
+
+ private:
+  RuntimeDiagnostics *runtime_;
+  GrayscaleDiagnostic *grayscale_;
+  TargetSearchDiagnostic *target_search_;
+};
+
+}  // namespace geometrie_camera_app
+}  // namespace esphome
