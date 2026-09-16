c04e98baa2137e47ea0a9d2d1ecebce6c0cfdf62
arousso59-prog <arousso59@icloud.com>
2026-09-16 16:40:29 +0200

    Add continuous measurement API interface

---
+++ components/geometrie_camera_app/continuous_measurement_api.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "esphome/components/web_server_base/web_server_base.h"
+
+namespace esphome {
+namespace geometrie_camera_app {
+
+class ContinuousMeasurementController;
+
+class ContinuousMeasurementApiHandler : public AsyncWebHandler {
+ public:
+  explicit ContinuousMeasurementApiHandler(ContinuousMeasurementController *controller);
+
+  bool canHandle(AsyncWebServerRequest *request) const override;
+  void handleRequest(AsyncWebServerRequest *request) override;
+
+ private:
+  bool parse_interval_(AsyncWebServerRequest *request, uint32_t &interval_ms,
+                       bool &present, std::string &error) const;
+  void handle_start_(AsyncWebServerRequest *request);
+  void handle_stop_(AsyncWebServerRequest *request);
+  void handle_status_(AsyncWebServerRequest *request) const;
+  void send_snapshot_(AsyncWebServerRequest *request, int response_code,
+                      const char *status, const char *error = nullptr) const;
+
+  ContinuousMeasurementController *controller_;
+};
+
+}  // namespace geometrie_camera_app
+}  // namespace esphome
