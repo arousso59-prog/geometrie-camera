e29c533dffb1235bf758d31fb53f635e2553a732
arousso59-prog <arousso59@icloud.com>
2026-09-16 08:36:17 +0200

    Extend OV5640 timing diagnostics with JPEG DVP controls

--- components/geometrie_camera_app/ov5640_timing_controller.h
+++ components/geometrie_camera_app/ov5640_timing_controller.h
@@ -14,9 +14,13 @@ struct Ov5640TimingSnapshot {
   bool pclk_manual;
   int hts;
   int vts;
+  int jpeg_mode;
+  int href_blanking;
   bool baseline_available;
   uint16_t baseline_hts;
   uint16_t baseline_vts;
+  uint8_t baseline_jpeg_mode;
+  uint8_t baseline_href_blanking;
 };
 
 class Ov5640TimingController {
@@ -27,22 +31,30 @@ class Ov5640TimingController {
   bool set_pclk_divider(uint8_t divider);
   bool set_hts(uint16_t hts);
   bool set_vts(uint16_t vts);
-  bool restore_total_timing();
+  bool set_jpeg_mode(uint8_t mode);
+  bool set_href_blanking(uint8_t blanking);
+  bool restore_baseline();
   bool baseline_available() const;
 
   static uint8_t min_pclk_divider();
   static uint8_t max_pclk_divider();
   static uint16_t min_total_timing();
   static uint16_t max_total_timing();
+  static uint8_t min_href_blanking();
+  static uint8_t max_href_blanking();
 
  private:
   bool capture_baseline_if_needed_();
   bool read_total_timing_(uint16_t *hts, uint16_t *vts) const;
+  bool read_jpeg_output_timing_(uint8_t *jpeg_mode, uint8_t *href_blanking) const;
   bool write_total_timing_register_(uint16_t high_register, uint16_t value) const;
+  bool write_byte_register_(uint16_t reg, uint8_t value) const;
 
   bool baseline_available_;
   uint16_t baseline_hts_;
   uint16_t baseline_vts_;
+  uint8_t baseline_jpeg_mode_;
+  uint8_t baseline_href_blanking_;
 };
 
 }  // namespace geometrie_camera_app
