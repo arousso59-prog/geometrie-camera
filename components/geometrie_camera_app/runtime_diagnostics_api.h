3572852e3cd79b36ceb0f246c5479d692bfc6bdd
arousso59-prog <arousso59@icloud.com>
2026-09-16 10:11:44 +0200

    Drop obsolete runtime diagnostic dependencies

--- components/geometrie_camera_app/runtime_diagnostics_api.h
+++ components/geometrie_camera_app/runtime_diagnostics_api.h
@@ -5,24 +5,19 @@
 namespace esphome {
 namespace geometrie_camera_app {
 
-class GrayscaleDiagnostic;
 class JpegDiagnostic;
 class RuntimeDiagnostics;
-class TargetSearchDiagnostic;
 
 class RuntimeDiagnosticsApiHandler : public AsyncWebHandler {
  public:
-  RuntimeDiagnosticsApiHandler(RuntimeDiagnostics *runtime, GrayscaleDiagnostic *grayscale, JpegDiagnostic *jpeg,
-                               TargetSearchDiagnostic *target_search);
+  RuntimeDiagnosticsApiHandler(RuntimeDiagnostics *runtime, JpegDiagnostic *jpeg);
 
   bool canHandle(AsyncWebServerRequest *request) const override;
   void handleRequest(AsyncWebServerRequest *request) override;
 
  private:
   RuntimeDiagnostics *runtime_;
-  GrayscaleDiagnostic *grayscale_;
   JpegDiagnostic *jpeg_;
-  TargetSearchDiagnostic *target_search_;
 };
 
 }  // namespace geometrie_camera_app
