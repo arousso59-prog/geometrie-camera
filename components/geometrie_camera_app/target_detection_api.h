e6ecd63ed98c6a276bcf336f471009b2bedb4b38
arousso59-prog <arousso59@icloud.com>
2026-09-16 11:13:17 +0200

    Expose preview cible dans API

--- components/geometrie_camera_app/target_detection_api.h
+++ components/geometrie_camera_app/target_detection_api.h
@@ -5,11 +5,15 @@
 namespace esphome {
 namespace geometrie_camera_app {
 
+class JpegFilteredDiagnostic;
+class TargetDetectionPreview;
 class TargetDetectionService;
 
 class TargetDetectionApiHandler : public AsyncWebHandler {
  public:
-  explicit TargetDetectionApiHandler(TargetDetectionService *service);
+  TargetDetectionApiHandler(TargetDetectionService *service,
+                            JpegFilteredDiagnostic *source,
+                            TargetDetectionPreview *preview);
 
   bool canHandle(AsyncWebServerRequest *request) const override;
   void handleRequest(AsyncWebServerRequest *request) override;
@@ -17,10 +21,13 @@ class TargetDetectionApiHandler : public AsyncWebHandler {
  private:
   void handle_detect_(AsyncWebServerRequest *request);
   void handle_status_(AsyncWebServerRequest *request) const;
+  void handle_preview_(AsyncWebServerRequest *request);
   void send_status_(AsyncWebServerRequest *request, int response_code, const char *status,
                     const char *error = nullptr) const;
 
   TargetDetectionService *service_;
+  JpegFilteredDiagnostic *source_;
+  TargetDetectionPreview *preview_;
 };
 
 }  // namespace geometrie_camera_app
