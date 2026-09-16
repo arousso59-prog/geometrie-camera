a97d9a4558dd9e60bd73207089ca693e9af3089a
arousso59-prog <arousso59@icloud.com>
2026-09-16 14:32:23 +0200

    Add target corners and geometry pose measurement types

--- components/geometrie_camera_app/types.h
+++ components/geometrie_camera_app/types.h
@@ -6,6 +6,13 @@
 namespace esphome {
 namespace geometrie_camera_app {
 
+struct ImagePoint {
+  ImagePoint();
+
+  float x;
+  float y;
+};
+
 struct CameraCalibration {
   CameraCalibration();
 
@@ -13,6 +20,8 @@ struct CameraCalibration {
   float fy_px;
   float cx_px;
   float cy_px;
+  uint16_t reference_width_px;
+  uint16_t reference_height_px;
 };
 
 struct TargetObservation {
@@ -25,12 +34,23 @@ struct TargetObservation {
   float height_px;
   float rotation_deg;
   float quality;
+  ImagePoint top_left_px;
+  ImagePoint top_right_px;
+  ImagePoint bottom_right_px;
+  ImagePoint bottom_left_px;
 };
 
 struct GeometryMeasurement {
   GeometryMeasurement();
 
   bool valid;
+  bool calibrated;
+  float distance_mm;
+  float x_mm;
+  float y_mm;
+  float z_mm;
+  float bearing_yaw_deg;
+  float bearing_pitch_deg;
   float yaw_deg;
   float pitch_deg;
   float roll_deg;
