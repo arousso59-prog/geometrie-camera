be71126ef5755f16a60f754d49242ecc5b74053d
arousso59-prog <arousso59@icloud.com>
2026-09-16 08:19:22 +0200

    Extend OV5640 timing controller with HTS and VTS diagnostics

--- components/geometrie_camera_app/ov5640_timing_controller.h
+++ components/geometrie_camera_app/ov5640_timing_controller.h
@@ -12,6 +12,11 @@ struct Ov5640TimingSnapshot {
   int pclk_divider;
   int vfifo_ctrl0c;
   bool pclk_manual;
+  int hts;
+  int vts;
+  bool baseline_available;
+  uint16_t baseline_hts;
+  uint16_t baseline_vts;
 };
 
 class Ov5640TimingController {
@@ -20,9 +25,24 @@ class Ov5640TimingController {
 
   Ov5640TimingSnapshot snapshot() const;
   bool set_pclk_divider(uint8_t divider);
+  bool set_hts(uint16_t hts);
+  bool set_vts(uint16_t vts);
+  bool restore_total_timing();
+  bool baseline_available() const;
 
   static uint8_t min_pclk_divider();
   static uint8_t max_pclk_divider();
+  static uint16_t min_total_timing();
+  static uint16_t max_total_timing();
+
+ private:
+  bool capture_baseline_if_needed_();
+  bool read_total_timing_(uint16_t *hts, uint16_t *vts) const;
+  bool write_total_timing_register_(uint16_t high_register, uint16_t value) const;
+
+  bool baseline_available_;
+  uint16_t baseline_hts_;
+  uint16_t baseline_vts_;
 };
 
 }  // namespace geometrie_camera_app
