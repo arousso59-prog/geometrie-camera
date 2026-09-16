19601a62ce7f2b74c39f5712834056ab0dfa711f
arousso59-prog <arousso59@icloud.com>
2026-09-16 08:21:50 +0200

    Document OV5640 HTS VTS timing test API

--- components/geometrie_camera_app/api_wsdl.cpp
+++ components/geometrie_camera_app/api_wsdl.cpp
@@ -25,7 +25,7 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
                                         : "unknown";
 
   std::string xml;
-  xml.reserve(18432);
+  xml.reserve(19456);
   xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
   xml += "<api name=\"geometrie-camera\" version=\"1\" style=\"REST-over-HTTP\">\n";
   xml += "  <description>Catalogue WSDL-like des routes HTTP du projet. Ce document doit etre maintenu avec toute evolution d API.</description>\n";
@@ -90,16 +90,25 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   xml += "  </method>\n";
 
   xml += "  <method name=\"camera_timing_get\" http=\"GET\" path=\"/api/camera/timing\">\n";
-  xml += "    <comment>Lit le timing DVP de l OV5640 utilise pour le diagnostic des artefacts JPEG : PID, diviseur PCLK 0x3824 et VFIFO_CTRL0C 0x460C.</comment>\n";
+  xml += "    <comment>Lit le timing bas niveau de l OV5640 : diviseur PCLK, VFIFO_CTRL0C, HTS 0x380C/0x380D, VTS 0x380E/0x380F et eventuelle reference memorisee pour restauration.</comment>\n";
   xml += "    <response code=\"200\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
   xml += "  </method>\n";
 
   xml += "  <method name=\"camera_timing_set\" http=\"GET\" path=\"/api/camera/timing/set\">\n";
-  xml += "    <comment>Modifie uniquement le diviseur PCLK manuel de l OV5640 et verifie la valeur relue. Un changement de resolution peut reprogrammer ce registre ; appliquer le timing apres avoir choisi la resolution.</comment>\n";
-  xml += "    <parameter name=\"pclk_divider\" location=\"query\" required=\"true\" type=\"integer\" allowed=\"1..31\">Diviseur du registre OV5640 0x3824.</parameter>\n";
+  xml += "    <comment>Modifie un ou plusieurs parametres de timing OV5640 avec readback. La premiere modification HTS/VTS memorise automatiquement les valeurs courantes pour pouvoir les restaurer.</comment>\n";
+  xml += "    <parameter name=\"pclk_divider\" location=\"query\" required=\"false\" type=\"integer\" allowed=\"1..31\">Diviseur PCLK du registre 0x3824.</parameter>\n";
+  xml += "    <parameter name=\"hts\" location=\"query\" required=\"false\" type=\"integer\" allowed=\"1..65535\">Horizontal Total Size, registres 0x380C/0x380D. Decimal ou notation 0x...</parameter>\n";
+  xml += "    <parameter name=\"vts\" location=\"query\" required=\"false\" type=\"integer\" allowed=\"1..65535\">Vertical Total Size, registres 0x380E/0x380F. Decimal ou notation 0x...</parameter>\n";
   xml += "    <response code=\"200\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"400\" content_type=\"application/json\"/>\n";
+  xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
+  xml += "  </method>\n";
+
+  xml += "  <method name=\"camera_timing_restore\" http=\"GET\" path=\"/api/camera/timing/restore\">\n";
+  xml += "    <comment>Restaure HTS/VTS aux valeurs memorisees juste avant la premiere modification de timing de la serie de test. Ne restaure pas le diviseur PCLK.</comment>\n";
+  xml += "    <response code=\"200\" content_type=\"application/json\"/>\n";
+  xml += "    <response code=\"409\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
   xml += "  </method>\n";
 
@@ -134,7 +143,7 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   xml += "    <comment>Purge la frame pre-acquise par ESPHome puis demande une frame JPEG fraiche produite par l ISP du capteur.</comment>\n";
   xml += "    <parameter name=\"resolution\" location=\"query\" required=\"false\" type=\"string\" allowed=\"";
   xml += allowed_resolutions;
-  xml += "\">Resolution a appliquer avant la capture JPEG. Le changement de resolution peut remettre le diviseur PCLK du driver ; regler /api/camera/timing/set ensuite pour un test timing.</parameter>\n";
+  xml += "\">Resolution a appliquer avant la capture JPEG. Un changement de resolution peut reprogrammer les registres de timing ; choisir la resolution avant un test HTS/VTS.</parameter>\n";
   xml += "    <response code=\"202\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"400\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
