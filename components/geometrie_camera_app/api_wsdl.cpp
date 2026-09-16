c452b13f3fbb2c2fb89fe1349af23d3437ff463f
arousso59-prog <arousso59@icloud.com>
2026-09-16 11:29:36 +0200

    Document TargetDetector V4 behavior

--- components/geometrie_camera_app/api_wsdl.cpp
+++ components/geometrie_camera_app/api_wsdl.cpp
@@ -27,12 +27,12 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   std::string xml;
   xml.reserve(13312);
   xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  xml += "<api name=\"geometrie-camera\" version=\"5\" style=\"REST-over-HTTP\">\n";
+  xml += "<api name=\"geometrie-camera\" version=\"6\" style=\"REST-over-HTTP\">\n";
   xml += "  <description>API de la voie camera validee : OV5640 JPEG, correction grayscale, detection cible, reglages et diagnostic runtime.</description>\n";
   xml += "  <conventions>\n";
   xml += "    <item>Les routes de commande de mise au point utilisent encore HTTP GET.</item>\n";
   xml += "    <item>La detection cible travaille uniquement sur la derniere image grayscale corrigee.</item>\n";
-  xml += "    <item>target_found indique l acceptation finale ; le bloc target expose le meilleur candidat meme s il reste sous le seuil.</item>\n";
+  xml += "    <item>target_found indique l acceptation finale ; le bloc target expose le meilleur candidat ayant passe les controles structurels internes.</item>\n";
   xml += "  </conventions>\n";
 
   xml += "  <method name=\"api_wsdl\" http=\"GET\" path=\"/api/wsdl\">\n";
@@ -106,14 +106,14 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   xml += "  </method>\n";
 
   xml += "  <method name=\"target_detect\" http=\"GET\" path=\"/target/detect\">\n";
-  xml += "    <comment>Lance TargetDetector V3 sur la derniere image grayscale corrigee. La V3 limite la plage de taille, moyenne localement les cellules et valide la coherence du cadre noir. Ne relance ni capture ni filtrage.</comment>\n";
+  xml += "    <comment>Lance TargetDetector V4 sur la derniere image grayscale corrigee. Le V4 classe uniquement les candidats ayant un cadre noir coherent et un fond clair immediat autour du marqueur ; le prefiltre global utilise des lectures simples pour reduire le temps CPU. Ne relance ni capture ni filtrage.</comment>\n";
   xml += "    <response code=\"200\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"409\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
   xml += "  </method>\n";
 
   xml += "  <method name=\"target_status\" http=\"GET\" path=\"/target/status\">\n";
-  xml += "    <comment>Relit le dernier resultat. Le bloc target contient toujours le meilleur candidat disponible, meme si target_found vaut false.</comment>\n";
+  xml += "    <comment>Relit le dernier resultat. Le bloc target contient le meilleur candidat ayant passe les controles structurels internes ; target_found indique s il depasse aussi le seuil final de qualite.</comment>\n";
   xml += "    <response code=\"200\" content_type=\"application/json\"/>\n";
   xml += "    <response code=\"500\" content_type=\"application/json\"/>\n";
   xml += "  </method>\n";
