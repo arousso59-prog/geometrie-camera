7ad4f57e3720b0ebd686680e3e1d6fff81417b5e
arousso59-prog <arousso59@icloud.com>
2026-09-16 09:22:30 +0200

    Add JPEG artifact corrector interface

---
+++ components/geometrie_camera_app/jpeg_artifact_corrector.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace esphome {
+namespace geometrie_camera_app {
+
+struct JpegArtifactCorrectionStats {
+  uint32_t green_seed_pixels;
+  uint32_t thin_green_pixels;
+  uint32_t corrected_green_pixels;
+  uint32_t corrected_dark_pixels;
+  uint32_t corrected_total_pixels;
+  uint16_t affected_rows;
+};
+
+class JpegArtifactCorrector {
+ public:
+  JpegArtifactCorrector();
+
+  bool is_green_seed(uint8_t red, uint8_t green, uint8_t blue) const;
+  bool correct(uint8_t *grayscale, size_t row_stride, const uint8_t *green_mask,
+               uint16_t width, uint16_t height);
+
+  const JpegArtifactCorrectionStats &stats() const;
+
+ private:
+  void reset_stats_();
+
+  JpegArtifactCorrectionStats stats_;
+};
+
+}  // namespace geometrie_camera_app
+}  // namespace esphome
