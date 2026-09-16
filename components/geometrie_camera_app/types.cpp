6ed87343e97ade80b617a471736eea1405ec67d2
arousso59-prog <arousso59@icloud.com>
2026-09-16 10:12:26 +0200

    Remove obsolete placeholder type constructors

--- components/geometrie_camera_app/types.cpp
+++ components/geometrie_camera_app/types.cpp
@@ -3,9 +3,6 @@
 namespace esphome {
 namespace geometrie_camera_app {
 
-CameraFrameInfo::CameraFrameInfo()
-    : valid(false), width(0), height(0), size_bytes(0), timestamp_ms(0) {}
-
 CameraCalibration::CameraCalibration()
     : fx_px(1000.0f), fy_px(1000.0f), cx_px(1024.0f), cy_px(768.0f) {}
 
@@ -17,9 +14,6 @@ GeometryMeasurement::GeometryMeasurement()
 
 GrayFrameView::GrayFrameView()
     : data(nullptr), width(0), height(0), stride(0) {}
-
-ImageBufferView::ImageBufferView()
-    : data(nullptr), size_bytes(0), width(0), height(0), mime_type(nullptr) {}
 
 }  // namespace geometrie_camera_app
 }  // namespace esphome
