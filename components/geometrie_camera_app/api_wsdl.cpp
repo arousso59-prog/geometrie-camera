efe818eb40dc82092eb0c90fb2a06acbffae4df3
arousso59-prog <arousso59@icloud.com>
2026-09-18 14:54:13 +0200

    Freeze validated camera and measurement method as v27

--- components/geometrie_camera_app/api_wsdl.cpp
+++ components/geometrie_camera_app/api_wsdl.cpp
@@ -19,13 +19,13 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   std::string xml;
   xml.reserve(7000);
   xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  xml += "<api name=\"geometrie-camera\" version=\"26\">\n";
+  xml += "<api name=\"geometrie-camera\" version=\"27\">\n";
   xml += "  <description>API operationnelle du capteur de geometrie. Le tracking haute precision et les reglages optiques sont automatiques.</description>\n";
   xml += "  <conventions>\n";
   xml += "    <item>Le tracking SEARCH vers PRECISE est permanent et ne possede plus de commande activation/desactivation.</item>\n";
   xml += "    <item>Les reglages camera sont internes a la calibration automatique et ne sont plus exposes en commande manuelle.</item>\n";
   xml += "    <item>Le pipeline image est capture JPEG, decodage gris, detection V6.1 puis mesure. Le controle de nettete et la correction d artefacts ont ete retires.</item>\n";
-  xml += "    <item>V26 conserve le verrouillage OV5640 V25 et evalue chaque candidat manuel sur plusieurs images afin de privilegier la repetabilite metrologique.</item>\n";
+  xml += "    <item>V27 fige le reglage camera valide V25 et la mesure officielle V6.1 avec stabilisation robuste sur 5 mesures. V5/V6 restent diagnostics uniquement.</item>\n";
   xml += "    <item>SEARCH, ZOOM_WIDE, ZOOM_MEDIUM et ZOOM_FINE servent uniquement au tracking. Seul PRECISE natif 800x600 produit une mesure.</item>\n";
   xml += "    <item>La calibration, la prise de mesure et le mode continu restent les trois usages operationnels conserves.</item>\n";
   xml += "  </conventions>\n";
@@ -42,7 +42,7 @@ void ApiWsdlHandler::handleRequest(AsyncWebServerRequest *request) {
   xml += "    <parameter name=\"target_size_mm\" location=\"query\" required=\"false\" type=\"number\"/>\n";
   xml += "  </method>\n";
   xml += "  <method name=\"measurement_compute\" http=\"GET\" path=\"/measurement/compute\"/>\n";
-  xml += "  <method name=\"measurement_status\" http=\"GET\" path=\"/measurement/status\"><comment>Inclut raw_measurement.precision_diag pour comparer V5, V6, V6.1, RMS et gradients.</comment></method>\n";
+  xml += "  <method name=\"measurement_status\" http=\"GET\" path=\"/measurement/status\"><comment>Methode officielle V6.1-robust5 figee; raw_measurement.precision_diag conserve V5/V6 en diagnostic.</comment></method>\n";
 
   xml += "  <method name=\"full_calibration_start\" http=\"GET\" path=\"/calibration/full/start\">\n";
   xml += "    <comment>Calibration autonome : tracking jusqu a PRECISE, auto-reglage optique, puis serie de mesures.</comment>\n";
