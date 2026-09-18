f04f6273cc3e25397d11727b6c29c61675567fa8
arousso59-prog <arousso59@icloud.com>
2026-09-18 10:11:03 +0200

    Expose V5 per-axis subpixel uncertainty

--- components/geometrie_camera_app/target_detection_api.cpp
+++ components/geometrie_camera_app/target_detection_api.cpp
@@ -92,7 +92,7 @@ void TargetDetectionApiHandler::handle_preview_(AsyncWebServerRequest *request) 
 void TargetDetectionApiHandler::send_status_(AsyncWebServerRequest *request, int response_code,
                                              const char *status, const char *error) const {
   std::string json;
-  json.reserve(980);
+  json.reserve(1250);
   json += "{\"status\":\"";
   json += status;
   json += "\"";
@@ -139,6 +139,14 @@ void TargetDetectionApiHandler::send_status_(AsyncWebServerRequest *request, int
     json += std::to_string(observation.subpixel_width_px);
     json += ",\"subpixel_height_px\":";
     json += std::to_string(observation.subpixel_height_px);
+    json += ",\"subpixel_width_sigma_px\":";
+    json += std::to_string(observation.subpixel_width_sigma_px);
+    json += ",\"subpixel_height_sigma_px\":";
+    json += std::to_string(observation.subpixel_height_sigma_px);
+    json += ",\"subpixel_width_gradient\":";
+    json += std::to_string(observation.subpixel_width_gradient);
+    json += ",\"subpixel_height_gradient\":";
+    json += std::to_string(observation.subpixel_height_gradient);
     json += "},\"preview\":\"/target/preview.bmp\"";
   }
 
