0ba35582a0c907c40802e8ed3a6c993c27b01b7f
arousso59-prog <arousso59@icloud.com>
2026-09-18 07:47:36 +0200

    Wire subpixel refiner into target detector

--- components/geometrie_camera_app/target_detector.h
+++ components/geometrie_camera_app/target_detector.h
@@ -3,6 +3,7 @@
 #include "target_candidate_finder.h"
 #include "target_code_decoder.h"
 #include "target_corner_refiner.h"
+#include "target_subpixel_refiner.h"
 #include "types.h"
 
 namespace esphome {
@@ -22,6 +23,7 @@ class TargetDetector {
 
   TargetCandidateFinder candidate_finder_;
   TargetCornerRefiner corner_refiner_;
+  TargetSubpixelRefiner subpixel_refiner_;
   TargetCodeDecoder code_decoder_;
   TargetCandidateSet candidates_;
 
